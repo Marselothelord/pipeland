5c50c97a1459159d9bb741f9dd106be1e455fc12
Marselothelord <manoseadordeparedes666@gmail.com>
2025-01-20 00:38:28 -0300

    Add files via upload

--- execute.sh
+++ execute.sh
@@ -7,19 +7,14 @@ echo "Cara de Verga"
 sudo cp ~/Files/pacman.conf /etc/pacman.conf
 
 ##instalar programas principales
-sudo pacman -S --noconfirm --needed waybar hyprland thunar swaync rofi-wayland swww slurp grim wl-clipboard thunar ffmpegthumbnailer thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler zed micro steam lutris git ark
-sudo pacman -R --noconfirm dolphin dolphin-plugins polkit-kde-agent wofi
+sudo pacman -S --noconfirm --needed waybar hyprland thunar swaync rofi-wayland swww slurp grim wl-clipboard thunar ffmpegthumbnailer thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler zed micro steam lutris git
 
 ##instalar yay
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 cd
 
 ##instalar vesktop y ATlauncher
-yay -S vesktop atlauncher hyprpolkitagent mercury-browser-bin --noconfirm
-yay -S all-repository-fonts --noconfirm
+yay -S vesktop atlauncher --noconfirm
 
 ##hacer magia
 sudo cp -r ~/Files/Walpape /home/martin/
-sudo cp ~/Files/hyprland.conf ~/.config/hypr/
-sudo cp -r ~/Files/kitty ~/.config/kitty
-sudo cp -r ~/Files/waybar /etc/xdg/waybar
