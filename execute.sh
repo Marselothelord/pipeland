ab416654ae11151408d540e95dcd24ba30f93fee
Marselothelord <manoseadordeparedes666@gmail.com>
2025-01-20 00:54:09 -0300

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -7,7 +7,7 @@ echo "Cara de Verga"
 sudo cp ~/pipeland/pacman.conf /etc/pacman.conf
 
 ##instalar programas principales
-sudo pacman -S --noconfirm --needed waybar hyprland thunar swaync rofi-wayland swww slurp grim wl-clipboard thunar ffmpegthumbnailer thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler zed micro steam lutris git ark 
+sudo pacman -S --noconfirm --needed waybar hyprland thunar swaync rofi-wayland swww slurp grim wl-clipboard thunar ffmpegthumbnailer thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler zed micro steam lutris git ark fastfetch 
 sudo pacman -R --noconfirm dolphin dolphin-plugins polkit-kde-agent wofi
 
 ##instalar yay
