ba3e8477fdf448782ed4f5747229bffa0203fde1
Marselothelord <manoseadordeparedes666@gmail.com>
2025-01-20 00:47:52 -0300

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -4,10 +4,11 @@
 echo "Cara de Verga"
 
 ##configuración de pacman
-sudo cp ~/Files/pacman.conf /etc/pacman.conf
+sudo cp ~/pipeland/pacman.conf /etc/pacman.conf
 
 ##instalar programas principales
-sudo pacman -S --noconfirm --needed waybar hyprland thunar swaync rofi-wayland swww slurp grim wl-clipboard thunar ffmpegthumbnailer thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler zed micro steam lutris git
+sudo pacman -S --noconfirm --needed waybar hyprland thunar swaync rofi-wayland swww slurp grim wl-clipboard thunar ffmpegthumbnailer thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler zed micro steam lutris git ark 
+sudo pacman -R --noconfirm dolphin dolphin-plugins polkit-kde-agent wofi
 
 ##instalar yay
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
@@ -17,4 +18,13 @@ cd
 yay -S vesktop atlauncher --noconfirm
 
 ##hacer magia
-sudo cp -r ~/Files/Walpape /home/martin/
+sudo cp -r ~/pipeland/Walpape /home/martin/
+sudo cp ~/pipeland/hyprland.conf ~/.config/hypr/
+sudo cp -r ~/pipeland/kitty ~/.config/kitty
+sudo cp -r ~/pipeland/waybar /etc/xdg/waybar
+
+##dar el aviso del preñao
+echo "Sistema Completamente PREÑADO. Reiniciando"
+
+##reiniciar sistema, pq te odio
+sudo reboot now
